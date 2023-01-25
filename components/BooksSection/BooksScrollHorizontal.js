2d5eb2100f60f67e363ebf64a978abfab390ac18
ayoub-jouini <ayoubjouini135@gmail.com>
2023-01-25 23:22:07 +0100

    BooksScrollHorizontal component

---
+++ components/BooksSection/BooksScrollHorizontal.js
@@ -0,0 +1,49 @@
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import colors from '../../styles/colors';
+import typography from '../../styles/typography';
+import BookPost from '../global/BookPost/BookPost';
+
+const BooksScrollHorizontal = ({ title, booksList }) => {
+  return (
+    <View style={styles.booksSection}>
+      <Text style={styles.booksContainerTitle}>{title}</Text>
+      <View>
+        <ScrollView
+          style={styles.booksContainer}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {booksList.map((book, key) => (
+            <BookPost
+              key={key}
+              image={book.image}
+              category={book.category}
+              name={book.name}
+              author={book.author}
+              rate={book.rate}
+              price={book.price}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  booksContainer: {
+    width: '100%',
+    height: 295,
+    paddingLeft: 20,
+  },
+  booksContainerTitle: {
+    fontFamily: 'Asap_400Regular',
+    color: colors.primary,
+    fontSize: typography.large,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+    marginVertical: 30,
+  },
+});
+
+export default BooksScrollHorizontal;
