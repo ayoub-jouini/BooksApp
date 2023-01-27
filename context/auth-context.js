0aaf072975a54cbb8fce17a18fc5cba59f8ef1be
ayoub-jouini <ayoubjouini135@gmail.com>
2023-01-27 14:38:24 +0100

    authContext

--- context/auth-context.js
+++ context/auth-context.js
@@ -1,39 +1,57 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createContext, useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext({
-  token: "",
+  token: '',
+  userId: '',
+  userAdress: '',
   isAuthenticated: false,
-  authenticate: (token) => {},
+  authenticate: (token, userId, userAdress) => {},
   logout: () => {},
 });
 
 const AuthContextProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState();
+  const [userId, setUserId] = useState();
+  const [userAdress, setUserAdress] = useState();
 
   useEffect(() => {
-    const fetchToken = async () => {
-      const storedToken = await AsyncStorage.getItem("token");
+    const fetchUserData = async () => {
+      const storedToken = await AsyncStorage.getItem('token');
+      const storedUserId = await AsyncStorage.getItem('userId');
+      const storedUserAdress = await AsyncStorage.getItem('userAdress');
 
-      if (storedToken) {
+      if (storedToken && storedUserId && storedUserAdress) {
         setAuthToken(storedToken);
+        setUserId(storedUserId);
+        setUserAdress(storedUserAdress);
       }
     };
-    fetchToken();
+    fetchUserData();
   }, []);
 
-  const authenticate = (token) => {
+  const authenticate = (token, userId, userAdress) => {
     setAuthToken(token);
-    AsyncStorage.setItem("token", token);
+    AsyncStorage.setItem('token', token);
+    setUserId(token);
+    AsyncStorage.setItem('userId', userId);
+    setUserAdress(token);
+    AsyncStorage.setItem('userAdress', userAdress);
   };
 
   const logout = () => {
     setAuthToken(null);
-    AsyncStorage.removeItem("token");
+    AsyncStorage.removeItem('token');
+    setUserId(null);
+    AsyncStorage.removeItem('userId');
+    setUserAdress(null);
+    AsyncStorage.removeItem('userAdress');
   };
 
   const value = {
     token: authToken,
+    userId: userId,
+    userAdress: userAdress,
     isAuthenticated: !!authToken,
     authenticate,
     logout,
